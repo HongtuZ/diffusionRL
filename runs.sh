b131391012eac327d7040be15e156afe93628f29
zhouhongtu <zhouhongtu@tongji.edu.cn>
2025-06-13 12:16:31 +0800

    modify --env to --dataset_id in minari

--- runs.sh
+++ runs.sh
@@ -1,24 +1,24 @@
 
 
 #gym
-python main.py --env walker2d-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --tag Reproduce --num_seed 1 --gpu '0' --num_qs 10
-python main.py --env walker2d-medium-replay-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env walker2d-medium-expert-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id walker2d-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --tag Reproduce --num_seed 1 --gpu '0' --num_qs 10
+python main.py --dataset_id walker2d-medium-replay-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id walker2d-medium-expert-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
 
-python main.py --env hopper-medium-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env hopper-medium-replay-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env hopper-medium-expert-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 0.05 --rho 1.5 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id hopper-medium-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id hopper-medium-replay-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id hopper-medium-expert-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 0.05 --rho 1.5 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
 
-python main.py --env halfcheetah-medium-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --q_tar lcb --rho 0 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env halfcheetah-medium-replay-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1  --q_tar lcb --rho 0 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env halfcheetah-medium-expert-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 0.1  --q_tar lcb --rho 0 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id halfcheetah-medium-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1 --q_tar lcb --rho 0 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id halfcheetah-medium-replay-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 1  --q_tar lcb --rho 0 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id halfcheetah-medium-expert-v2 --agent dac --eta 1 --eta_lr 0.001 --bc_threshold 0.1  --q_tar lcb --rho 0 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
 
 #antmaze
-python main.py --env antmaze-umaze-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env antmaze-umaze-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --tag Reproduce  --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id antmaze-umaze-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id antmaze-umaze-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --tag Reproduce  --num_seed 8 --gpu '0, 1' --num_qs 10
 
-python main.py --env antmaze-medium-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0  --rho 1 --q_tar lcb  --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env antmaze-medium-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb   --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id antmaze-medium-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0  --rho 1 --q_tar lcb  --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id antmaze-medium-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb   --tag Reproduce --num_seed 8 --gpu '0, 1' --num_qs 10
 
-python main.py --env antmaze-large-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0  --rho 1.1 --q_tar lcb  --tag Reproduce   --critic_lr 0.001 --num_seed 8 --gpu '0, 1' --num_qs 10
-python main.py --env antmaze-large-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb   --tag Reproduce   --critic_lr 0.001 --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id antmaze-large-play-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0  --rho 1.1 --q_tar lcb  --tag Reproduce   --critic_lr 0.001 --num_seed 8 --gpu '0, 1' --num_qs 10
+python main.py --dataset_id antmaze-large-diverse-v0 --agent dac --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb   --tag Reproduce   --critic_lr 0.001 --num_seed 8 --gpu '0, 1' --num_qs 10
