218cdf3fe20a5efa63ff3ad1e285bbb8bcd4595c
zhouhongtu <zhouhongtu@tongji.edu.cn>
2025-08-05 17:19:27 +0800

    modify runs.sh

--- runs.sh
+++ runs.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
-agent="udac"
+
+# 检查参数数量
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <env> <gpu> <agent>"
+    echo "Example: $0 hopper 2 dac"
+    exit 1
+fi
+
+env=$1
+agent=$3
+gpu=$2
 num_seed=4
-gpu='0'
-#gym
-python main.py --env walker2d-medium-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb  --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env walker2d-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env walker2d-medium-replay-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env walker2d-medium-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
 
-python main.py --env hopper-medium-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env hopper-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env hopper-medium-replay-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env hopper-medium-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 0.05 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-
-python main.py --env halfcheetah-medium-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env halfcheetah-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env halfcheetah-medium-replay-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1  --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env halfcheetah-medium-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 0.1  --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-
-#antmaze
-python main.py --env antmaze-umaze-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env antmaze-umaze-diverse-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-
-python main.py --env antmaze-medium-play-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0  --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env antmaze-medium-diverse-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --num_seed $num_seed --gpu $gpu --num_qs 10
-
-python main.py --env antmaze-large-play-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0  --rho 1.1 --q_tar lcb --critic_lr 0.001 --num_seed $num_seed --gpu $gpu --num_qs 10
-python main.py --env antmaze-large-diverse-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --critic_lr 0.001 --num_seed $num_seed --gpu $gpu --num_qs 10
+case $env in
+    "walker")
+        python main.py --env walker2d-medium-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb  --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env walker2d-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env walker2d-medium-replay-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env walker2d-medium-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        ;;
+    "hopper")
+        python main.py --env hopper-medium-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env hopper-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env hopper-medium-replay-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env hopper-medium-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 0.05 --rho 1.5 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        ;;
+    "halfcheetah")
+        echo "Running experiments for HalfCheetah"
+        python main.py --env halfcheetah-medium-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env halfcheetah-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1 --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env halfcheetah-medium-replay-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 1  --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env halfcheetah-medium-expert-v2 --agent $agent --eta 1 --eta_lr 0.001 --bc_threshold 0.1  --q_tar lcb --rho 0 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        ;;
+    "antmaze")
+        python main.py --env antmaze-umaze-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env antmaze-umaze-diverse-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env antmaze-medium-play-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0  --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env antmaze-medium-diverse-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env antmaze-large-play-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0  --rho 1.1 --q_tar lcb --critic_lr 0.001 --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        python main.py --env antmaze-large-diverse-v0 --agent $agent --maxQ --eta 0.1 --eta_lr 0 --rho 1 --q_tar lcb --critic_lr 0.001 --num_seed $num_seed --gpu "$gpu" --num_qs 10
+        ;;
+    *)
+        echo "Unknown environment: $env should be one of {walker, hopper, halfcheetah, antmaze}"
+        exit 1
+        ;;
+esac
